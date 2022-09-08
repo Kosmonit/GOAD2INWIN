56776cfb4ba5736c316af1fda57b65c0700ad652
Kirill <dolphin_d@bk.ru>
2022-09-08 12:59:21 +0300

    Add files via upload
    Patch for install Windows Server 2016 & 2019

---
+++ vagrant/!prepare_2019.ps1
@@ -0,0 +1,7 @@
+.\Install-WMF3Hotfix.ps1
+.\ConfigureRemotingForAnsible.ps1 
+
+Install-Module -Name ComputerManagementDsc
+Install-Module -Name xNetworking
+Install-Module -Name xDnsServer
+Install-Module -Name xActiveDirectory 
