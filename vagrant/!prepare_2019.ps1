4f4525bccd6760f2814e1299b0c0cd326bd3776a
Kirill <dolphin_d@bk.ru>
2022-09-08 13:09:49 +0300

    Delete !prepare_2019.ps1

--- vagrant/!prepare_2019.ps1
+++
@@ -1,7 +0,0 @@
-.\Install-WMF3Hotfix.ps1
-.\ConfigureRemotingForAnsible.ps1 
-
-Install-Module -Name ComputerManagementDsc
-Install-Module -Name xNetworking
-Install-Module -Name xDnsServer
-Install-Module -Name xActiveDirectory 
